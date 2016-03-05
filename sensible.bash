3848b1588eabfa72354aaf0fdd9aab829b81672d
Derek Gottlieb <derek.gottlieb@gmail.com>
2016-03-05 07:03:13 -0600

    Redirecting stderr when trying to set dirspell since not present on OS X version of bash

--- sensible.bash
+++ sensible.bash
@@ -62,7 +62,7 @@ bind '"\e[D": backward-char'
 # Prepend cd to directory names automatically
 shopt -s autocd 2> /dev/null
 # Correct spelling errors during tab-completion
-shopt -s dirspell 
+shopt -s dirspell 2> /dev/null
 # Correct spelling errors in arguments supplied to cd
 shopt -s cdspell 2> /dev/null
 
