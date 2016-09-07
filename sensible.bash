5a2269a6a12e2a1b10629bb223f2f3c27ac07050
mrzool <zool.bib@gmail.com>
2016-09-07 12:20:42 +0200

    Immediately add a trailing slash when autocompleting symlinks to directories

--- sensible.bash
+++ sensible.bash
@@ -44,6 +44,9 @@ bind "set completion-map-case on"
 # Display matches for ambiguous patterns at first tab press
 bind "set show-all-if-ambiguous on"
 
+# Immediately add a trailing slash when autocompleting symlinks to directories
+bind "set mark-symlinked-directories on"
+
 ## SANE HISTORY DEFAULTS ##
 
 # Append to the history file, don't overwrite it
